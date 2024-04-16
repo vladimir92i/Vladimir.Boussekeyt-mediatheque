a4ad6ad748352b92d63295e5df11bb09240ce024
Vladimir <v.boussekeyt@ecoles-epsi.net>
2024-04-16 13:47:47 +0200

    first commit

---
+++ MediathequeSolution/Mediatheque.Core/Service/MediathequeService.cs
@@ -0,0 +1,25 @@
+﻿using Mediatheque.Core.Model;
+
+namespace Mediatheque.Core.Service
+{
+    public class MediathequeService
+    {
+        private List<ObjetDePret> _fondDeCommerce = new List<ObjetDePret>();
+        private INotationService _notationService;
+
+        public MediathequeService(INotationService notationService)
+        {
+            _notationService = notationService;
+        }
+
+        public void AddObjet(ObjetDePret objet)
+        {
+            _fondDeCommerce.Add(objet);
+        }
+
+        public int GetNombreObjet()
+        {
+            return _fondDeCommerce.Count;
+        }
+    }
+}
